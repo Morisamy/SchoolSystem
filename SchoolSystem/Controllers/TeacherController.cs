b50795ad7cdd7ec46a76a8e5c6a82e4701a13aab
Samy <Samy@Laptop>
2022-09-15 13:00:31 +0200

    Completed Create + Edit  Operations

--- SchoolSystem/Controllers/TeacherController.cs
+++ SchoolSystem/Controllers/TeacherController.cs
@@ -9,11 +9,39 @@ namespace SchoolSystem.Controllers
         HRDatabaseContext dbContext = new HRDatabaseContext();
         public IActionResult Index()
         {
-            List<Teacher> teachers = dbContext.Teachers.ToList();
+            //List<Teacher> teachers = dbContext.Teachers.ToList();
+            var teachers = (from Teacher in dbContext.Teachers
+                            join Department in dbContext.Departments
+                            on Teacher.TeacherDepartmentId equals Department.DepartmentId
+                            select new Teacher
+                            {
+                                TeacherId = Teacher.TeacherId,
+                                TeacherName = Teacher.TeacherName,
+                                TeacherDoB = Teacher.TeacherDoB,
+                                TeacherDoH = Teacher.TeacherDoH,
+                                TeacherSalary = Teacher.TeacherSalary,
+                                TeacherDepartmentId = Teacher.TeacherDepartmentId,
+                                TeacherDepartmentName = Department.DepartmentName
+                            }).ToList();
             return View(teachers);
         }
         public IActionResult Create()
         {
+            ViewBag.Departments = this.dbContext.Departments.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Teacher model)
+        {
+            ModelState.Remove("TeacherId");
+            ModelState.Remove("DepartmentId");
+            ModelState.Remove("DepartmentName");
+            if (ModelState.IsValid)
+            {
+                dbContext.Teachers.Add(model);
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
             ViewBag.Departments = this.dbContext.Departments.ToList();
             return View();
         }
@@ -22,6 +50,21 @@ namespace SchoolSystem.Controllers
             Teacher teacher = this.dbContext.Teachers.Where(e => e.TeacherId == id).FirstOrDefault();
             ViewBag.Departments = this.dbContext.Departments.ToList();
             return View("Create",teacher);
+        }
+        [HttpPost]
+        public IActionResult Edit(Teacher model)
+        {
+            ModelState.Remove("TeacherId");
+            ModelState.Remove("DepartmentId");
+            ModelState.Remove("DepartmentName");
+            if (ModelState.IsValid)
+            {
+                dbContext.Teachers.Update(model);
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Departments = this.dbContext.Departments.ToList();
+            return View("Create",model);
         }
     }
 }
