bcaf94f045a9aede879f056e5f7d800a08c245b5
Samy <Samy@Laptop>
2022-09-17 12:13:33 +0200

    on Teachers (CRUD 100%+ Sort and Search is 50%) (Students - Classes ) to be Added

--- SchoolSystem/Controllers/TeacherController.cs
+++ SchoolSystem/Controllers/TeacherController.cs
@@ -4,12 +4,24 @@ using SchoolSystem.Models;
 
 namespace SchoolSystem.Controllers
 {
+    public enum SortDirection
+    {
+        Ascending,
+        Descending
+    }
     public class TeacherController : Controller
     {
         HRDatabaseContext dbContext = new HRDatabaseContext();
-        public IActionResult Index()
+        public IActionResult Index(string SortField, string CurrentSortField, SortDirection SortDirection, string searhByName)
         {
-            //List<Teacher> teachers = dbContext.Teachers.ToList();
+            var teachers = getTeachers();
+            if(!string.IsNullOrEmpty(searhByName))
+                teachers = teachers.Where( e => e.TeacherName.ToLower().Contains(searhByName.ToLower())).ToList();
+            return View(this.sortTeachers(teachers,SortField,CurrentSortField,SortDirection)); 
+        }
+
+        private List<Teacher> getTeachers()
+        {
             var teachers = (from Teacher in dbContext.Teachers
                             join Department in dbContext.Departments
                             on Teacher.TeacherDepartmentId equals Department.DepartmentId
@@ -23,8 +35,9 @@ namespace SchoolSystem.Controllers
                                 TeacherDepartmentId = Teacher.TeacherDepartmentId,
                                 TeacherDepartmentName = Department.DepartmentName
                             }).ToList();
-            return View(teachers);
+            return teachers;
         }
+
         public IActionResult Create()
         {
             ViewBag.Departments = this.dbContext.Departments.ToList();
@@ -45,9 +58,9 @@ namespace SchoolSystem.Controllers
             ViewBag.Departments = this.dbContext.Departments.ToList();
             return View();
         }
-        public IActionResult Edit(int id)
+        public IActionResult Edit(int Id)
         {
-            Teacher teacher = this.dbContext.Teachers.Where(e => e.TeacherId == id).FirstOrDefault();
+            Teacher teacher = this.dbContext.Teachers.Where(e => e.TeacherId == Id).FirstOrDefault();
             ViewBag.Departments = this.dbContext.Departments.ToList();
             return View("Create",teacher);
         }
@@ -65,6 +78,39 @@ namespace SchoolSystem.Controllers
             }
             ViewBag.Departments = this.dbContext.Departments.ToList();
             return View("Create",model);
+        }
+        public IActionResult Delete(int Id)
+        {
+            Teacher teacher = this.dbContext.Teachers.Where(e => e.TeacherId == Id).FirstOrDefault();
+            if (teacher != null)
+            {
+                dbContext.Teachers.Remove(teacher);
+                dbContext.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        private List<Teacher> sortTeachers(List<Teacher> teachers, string sortField, string currentSortField, SortDirection sortDirection)
+        {
+            if(string.IsNullOrEmpty(sortField))
+            {
+                ViewBag.SortField = "TeacherName";
+                ViewBag.SortDirection = SortDirection.Ascending;
+            }
+            else
+            {
+                if (currentSortField == sortField)
+                    ViewBag.SortDireciton = sortDirection == SortDirection.Ascending ? SortDirection.Descending : SortDirection.Ascending;
+                
+                else
+                    ViewBag.SortDirection = SortDirection.Ascending;
+                ViewBag.SortField = sortField;
+            }
+            var propertyinfo = typeof(Teacher).GetProperty(ViewBag.SortField);
+            if(ViewBag.SortDirection == SortDirection.Ascending)
+                teachers = teachers.OrderBy(e => propertyinfo.GetValue(e,null)).ToList();
+            else
+                teachers = teachers.OrderByDescending(e => propertyinfo.GetValue(e, null)).ToList();
+            return teachers;
         }
     }
 }
